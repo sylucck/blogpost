35cfe5cbc4ef0a09fce6afe1594c40e8975374d3
sylvester <nkwontasylvester15@gmail.com>
2022-02-13 17:28:50 -0900

    more commit

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from blog import views as user_views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('profile/', user_views.profile, name="profile"),
+    path('summernote/', include('django_summernote.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
